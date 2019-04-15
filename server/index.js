5ce0840185b0001d00baa1de261364b5c5af4c8a
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-15 18:22:27 +0300

    Added more requirements for the project.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
-//Enables the .env file, therefore add a env property to process object.
-//Recommend to require it at the top of the file
+// Enables the .env file, therefore add a env property to process object.
+// Recommend to require it at the top of the file
 require('dotenv').config();
+
+// Middlewares
+
+const bodyParser = require("body-parser");
+
+//Require the session for saving user data and giving a user a unique experience.
+const session = require('express-session');
+
+//Use cors for enable cross origin requests
+const cors = require('cors');
