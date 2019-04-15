de077c3f49d3f3a4bcb190b29fde2fbef0fc01e8
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-15 18:24:35 +0300

    Imported the controllers.

--- server/index.js
+++ server/index.js
@@ -2,12 +2,23 @@
 // Recommend to require it at the top of the file
 require('dotenv').config();
 
-// Middlewares
+/* Middlewares */
 
 const bodyParser = require("body-parser");
 
-//Require the session for saving user data and giving a user a unique experience.
+// Require the session for saving user data and giving a user a unique experience.
 const session = require('express-session');
 
-//Use cors for enable cross origin requests
+// Use cors for enable cross origin requests
 const cors = require('cors');
+
+/* Controllers */
+
+// Set your admin functionality
+const adminController = require('./controllers/admin_controller');
+// Set your cloudinary functionality
+const cloudinaryController = require('./controllers/cloudinary_controller');
+// Set your user functionality.
+const userController = require('./controllers/user_controller');
+// Set your products functionality.
+const productsController = require('./controllers/products_controller');
