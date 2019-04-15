7adc8fe79b108b549e601dfbe9247ad9d9e9e716
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-15 18:35:57 +0300

    Created the admin endpoints.

--- server/index.js
+++ server/index.js
@@ -93,3 +93,14 @@ app.get('/api/products', productsController.readAllProducts);
 // Getting a specified product
 // Use a request parameter, since retrieving a specified product..
 app.get('/api/products/:id', productsController.readProduct);
+
+
+// Admin Endpoints
+// Gets the admin users.
+app.get('/api/users', adminController.getAdminUsers);
+// When a admin creates a product. No need for request parameter in this case. Since we are inserting data to database.
+app.post('/api/products', adminController.createProduct);
+// When a admin update a current product. Need request parameter since updating a specific product based on  the id.
+app.put('/api/products/:id', adminController.updateProduct);
+// When a admin deletes a product, need an id to specify a product to delete.
+app.delete('/api/products/:id', adminController.deleteProduct);
