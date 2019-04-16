34de5d19f47d74bb5c4ce6f7e2b300dd974b203e
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-17 00:54:44 +0300

    Implemented componentDidMount() method in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-import Home from'./components/container/Home/Home';
+// import Home from'./components/container/Home/Home';
 import Navbar from'./components/container/Navbar/Navbar';
 import routes from './routes';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { login, logout } from './redux/reducer';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+    componentDidMount() {                           // In it's componentDidMount get the session, and if it has data set your intialState user to it.
+        axios.get('/api/user-data')
+            .then(res => {
+                const { dispatch } = this.props;    // Destruct the dispatch props from this.props that will be responsible for dispatching or initiating actions from your reducer.
+                if(res.data.user) {                 // Dispatch the login function with the user data.
+                    dispatch(login(res.data.user));
+                } else {                            // Else logout the user from the intialState.
+                    dispatch(logout());             // Dispatch the logout the user by default if there is no data in session.
+                }
+            })
+    }
     render() {
         return (
             <div className="App">
@@ -15,4 +30,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default withRouter(connect()(App));          // Connect your app to your reducer.
