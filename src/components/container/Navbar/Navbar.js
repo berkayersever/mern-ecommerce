2573fbc18ecfea46a8eabe947dab55ea33853628
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-16 18:47:08 +0300

    Implemented the Navbar Component.

--- src/components/container/Navbar/Navbar.js
+++ src/components/container/Navbar/Navbar.js
@@ -1,3 +1,18 @@
 import React, { Component } from 'react';
 
 import './Navbar.css';      // Import the css file for styling.
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div className='nav container'>
+                <div className='desktop-nav'>
+                    <p className="nav-link">Home</p>
+                    <p className="nav-link">About</p>
+                    <p className="nav-link">Cart</p>
+                    <p className="nav-link">Login</p>
+                </div>
+            </div>
+        );
+    }
+}
