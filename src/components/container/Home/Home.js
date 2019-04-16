ccc9e4f156e989229765a131bd8493bc3b34b8ef
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-16 19:19:11 +0300

    Implemented Home.js

--- src/components/container/Home/Home.js
+++ src/components/container/Home/Home.js
@@ -1,1 +1,11 @@
 import React, { Component } from 'react';
+
+export default class Home extends Component {
+    constructor() {
+        super();
+        this.state = {      // Have a products and loading
+            products: [],   // products holding all your products
+            loading: true   // loading for when loader is shown. Set it true, and when data is retrieve set to false.
+        }
+    }
+}
